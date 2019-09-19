a133ba725959141de0e373f7103c92ba6f647954
Jesse <459659168@qq.com>
2019-09-19 16:23:25 +0800

    Updates

---
+++ precache-manifest.074cc358a9f86e22080cd1af0e1daf38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8934efdfd5a3379a556f95fe017db43d",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "2fc1612ef9559efd8fc7",
+    "url": "/react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ce9b0c29f44d3ccbb3fb",
+    "url": "/react-app/static/js/2.90aca201.chunk.js"
+  },
+  {
+    "revision": "2fc1612ef9559efd8fc7",
+    "url": "/react-app/static/js/main.e595f4f1.chunk.js"
+  },
+  {
+    "revision": "5b63f23dcb2dc5a0f139",
+    "url": "/react-app/static/js/runtime~main.b219130a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
